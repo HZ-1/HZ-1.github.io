48bf7cbf4ad4f0ca98397fa6acb0faa668744411
hz-qq2 <601661706@qq.com>
2018-11-18 14:47:33 +0800

    slector的多种写法

--- src/redux/fa.jsx
+++ src/redux/fa.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import {connect} from '../../node_modules/react-redux/lib';
 import {tyyReducer} from "./tyyReducer";
+import { bindActionCreators } from 'redux'
+
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -49,47 +52,93 @@ this.num =1;
   }
 }
 let num = 1;
-const mapDispatchToProps = (dispatch)=>{
-  return {
+
+//写法一 传统写法
+// const mapDispatchToProps = (dispatch)=>{
+//   return {
+//     abc:aaa=>{
+//         // console.log('ADD')
+//       dispatch({
+//         type:'ADD',
+//         id:num++,
+//         text:aaa,
+//
+//       })
+//     },
+//       qqw:aaa=>{
+//           // console.log('TYY')
+//           dispatch({
+//               type:'TYY',
+//               id:num++,
+//               text:aaa,
+//
+//           })
+//       },
+//       noa:aaa=>{
+//           // console.log('nono')
+//           dispatch({
+//               type:'nono',
+//               id:num++,
+//               text:aaa,
+//
+//           })
+//       }
+//   }
+// }
+
+const faActionCreators = {
     abc:aaa=>{
-        // console.log('ADD')
-      dispatch({
-        type:'ADD',
+        console.log(9232666);
+        return {
+            type:'ADD',
+            id:num++,
+            text:aaa,
+        }
+    },
+    qqw:aaa=>({
+        type:'TYY',
         id:num++,
         text:aaa,
-
-      })
-    },
-      qqw:aaa=>{
-          // console.log('TYY')
-          dispatch({
-              type:'TYY',
-              id:num++,
-              text:aaa,
-
-          })
-      },
-      noa:aaa=>{
-          // console.log('nono')
-          dispatch({
-              type:'nono',
-              id:num++,
-              text:aaa,
-
-          })
-      }
-  }
+    }),
+    noa:aaa=>({
+        type:'nono',
+        id:num++,
+        text:aaa,
+    })
 }
-const mapStateToProps = (state)=>{
-    console.log(state.addRedux[0])
-    console.log(state.tyyReducer)
-    // console.log('fa---mapStateToProps')
-  return ({addRedux:state.addRedux})
+
+//写法二
+// const mapDispatchToProps = faActionCreators;
+
+
+
+//写法三
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators(faActionCreators, dispatch);
 };
-// const mapStateToProps = ({addRedux})=>{
-//     console.log('fa---mapStateToProps')
-//     return ({addRedux})
+
+
+
+//mapStateToProps第一种写法结束-------
+const mapStateToProps = (state)=>{
+    return ({addRedux:state.addRedux})
+};
+
+//mapStateToProps第二种写法开始------
+//mapStateToProps的两种写法---第二种写法，
+//mapStateToPropsCreator 其实就是正常写法、第一种写法的mapStateToProps
+// const mapStateToPropsCreator = (state)=>{
+//     return ({addRedux:state.addRedux})
 // };
+//
+// //这种写法的好处就是能够在每次执行mapStateToProps都可以使用初始状态initstate
+// const mapStateToProps = (initstate,ownprops)=>{
+//     console.log(initstate);
+//     return mapStateToPropsCreator;
+// };
+//mapStateToProps第二种写法结束-------
+
+
 
 // export default connect(mapStateToProps,null)(App);
 // export default connect(null,mapDispatchToProps)(App);
